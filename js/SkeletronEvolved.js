dcc6d8f62e9663eb7b817ada5b1714719206c97a
Bertin Colpron <bcolpron78@gmail.com>
2017-03-05 16:14:46 -0500

    olivier

--- js/SkeletronEvolved.js
+++ js/SkeletronEvolved.js
@@ -7,7 +7,7 @@ var SkeletronEvolved = inherit(Ennemy, function(x, y, controller) {
                        {x:x+48,y:y+12}, {x:x+60,y:y+24}, {x:x+48,y:y+12}, {x:x+36,y:y+24}, {x:x+48,y:y+12} ]);
 });
 
-SkeletronEvolved.prototype.MAX_LIFE = 30;
+SkeletronEvolved.prototype.MAX_LIFE = 100;
 
 SkeletronEvolved.prototype.extents = [[0,0,1,0,0],
                                       [1,1,1,1,1],
