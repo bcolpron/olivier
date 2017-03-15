e5205d0c0561ea5367af02e5cda462b34d45b042
Bertin Colpron <bcolpron78@gmail.com>
2017-03-15 19:06:33 -0400

    enlarge moonlord

--- js/Moonlord.js
+++ js/Moonlord.js
@@ -5,7 +5,14 @@ var Moonlord = inherit(Boss, function(x, y, controller) {
     this.setMovements([{x:x,y:y}, {x:x+12,y:y-12}, {x:x+24,y:y}, {x:x+12,y:y+12} ]);
 })
 
-Moonlord.prototype.extents = [[1]];
+Moonlord.prototype.extents = [[0,0,0,0,0,0,0,0],
+                              [0,0,0,1,1,0,0,0],
+                              [0,1,1,1,1,1,1,0],
+                              [0,0,0,1,1,0,0,0],
+                              [0,0,0,1,1,0,0,0],
+                              [0,0,0,0,0,0,0,0],
+                              [0,0,0,0,0,0,0,0],
+                              [0,0,0,0,0,0,0,0]];
 
 Moonlord.prototype.MAX_LIFE = 170;
 
